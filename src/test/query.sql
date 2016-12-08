1ba8301359435adbd0d506fd537e466a54b4845a
KuanYu Chu <ckybonist@gmail.com>
2016-12-08 20:26:05 +0800

    Update sql query example

--- src/test/query.sql
+++ src/test/query.sql
@@ -1,3 +1,6 @@
+USE Ragnarok;
+
+-- Find which monster have `Orange Potion` by INNER JOIN
 SELECT mobs.kROName, items.Name FROM items
     INNER JOIN mobs
     ON ItemId = mobs.Drop1Id
@@ -8,9 +11,66 @@ SELECT mobs.kROName, items.Name FROM items
         OR ItemId = mobs.Drop6Id
         OR ItemId = mobs.Drop7Id
         OR ItemId = mobs.Drop8Id
-    where items.Name='Orange Potion';
+    WHERE items.Name='Orange Potion';
+
+-- Create View with item data and mob data
+CREATE OR REPLACE VIEW MobItemsView AS
+    SELECT
+        mobs.kROName AS MonsterName,
+        GROUP_CONCAT(items.Name SEPARATOR ',  ') AS DroppedItems
+    FROM items
+    INNER JOIN mobs
+        ON ItemId IN (
+            mobs.Drop1id,
+            mobs.Drop2id,
+            mobs.Drop3id,
+            mobs.Drop4id,
+            mobs.Drop5id,
+            mobs.Drop6id,
+            mobs.Drop7id,
+            mobs.Drop8id,
+            mobs.Drop9id,
+            mobs.DropCardId
+        )
+    GROUP BY mobs.kROName
+    ORDER BY mobs.kROName;
+
+-- Find all monsters that will drop `Orange potion`
+SELECT MonsterName FROM MobItemsView
+    WHERE DroppedItems LIKE '%Orange Potion%';
 
 
-#SELECT mobs.kROName, items.Name FROM items
-#    INNER JOIN mobs
-#    ON ItemId = mobs.Drop1Id;
+-- Find which monster have `Orange Potion` by INNER JOIN
+-- However with a little bit tweaking.
+SELECT
+GROUP_CONCAT(mobs.kROName SEPARATOR ',  ') AS MonsterName,
+items.Name AS DroppedItem
+FROM items
+    INNER JOIN mobs
+    ON ItemId IN (
+        mobs.Drop1id,
+        mobs.Drop2id,
+        mobs.Drop3id,
+        mobs.Drop4id,
+        mobs.Drop5id,
+        mobs.Drop6id,
+        mobs.Drop7id,
+        mobs.Drop8id,
+        mobs.Drop9id,
+        mobs.DropCardId
+    )
+    WHERE items.Name='Orange Potion'
+    GROUP BY mobs.kROName;
+
+
+-- Find columns with name like `Dropxxxid` from mobs table
+-- SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
+--     WHERE table_name='mobs'
+--     AND table_schema = 'Ragnarok'
+--     AND column_name LIKE 'Drop%id';
+
+
+-- List buy and sell price of potions
+SELECT id, Name, BuyPrice, SellPrice
+    FROM items
+    WHERE Name LIKE '% Potion';
