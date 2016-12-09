ebc27a0b92c1b524ea98e9abc2d2c1ec98c93ac3
KuanYu Chu <ckybonist@gmail.com>
2016-12-09 11:35:26 +0800

    Fix query condition

--- src/test/inner-join.sql
+++ src/test/inner-join.sql
@@ -4,11 +4,13 @@ USE Ragnarok;
 SELECT mobs.kROName, items.Name FROM items
     INNER JOIN mobs
     ON ItemId = mobs.Drop1Id
-        OR ItemId = mobs.Drop2Id
-        OR ItemId = mobs.Drop3Id
-        OR ItemId = mobs.Drop4Id
-        OR ItemId = mobs.Drop5Id
-        OR ItemId = mobs.Drop6Id
-        OR ItemId = mobs.Drop7Id
-        OR ItemId = mobs.Drop8Id
+        OR ItemId = mobs.Drop2id
+        OR ItemId = mobs.Drop3id
+        OR ItemId = mobs.Drop4id
+        OR ItemId = mobs.Drop5id
+        OR ItemId = mobs.Drop6id
+        OR ItemId = mobs.Drop7id
+        OR ItemId = mobs.Drop8id
+        OR ItemId = mobs.Drop9id
+        OR ItemId = mobs.DropCarodId
     WHERE items.Name='Orange Potion';
